27bd50fbe1dac674d5681083f82d36841f7bcc77
EdgarCostilla1 <>
2022-02-17 08:43:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let pokemonType = "agua"
+if (pokemonType == "electrico"){
+  const pokemonName = "pikachu"
+  const poke = {
+    name: "pikachu"
+  }
+  poke.name = "raichu"
+  pokemonType = "electrico"
+  console.log(`mi pokemon es ${pokemonType} y se llama ${poke.name}`)
+}
+
+
+let count = 1;
+
+console
